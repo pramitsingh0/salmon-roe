e49b0633546217f76436452d951dda09320f8419
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-02 10:25:01 +0530

    [server]Login & Logout Implemented

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const imageUpload = require("../services/imageUpload");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../services/config");
 
 const createAccount = async (req, res, next) => {
   try {
@@ -21,6 +23,45 @@ const createAccount = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(401).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(401).json({ message: "Inavlid Credentials" });
+    }
+    let token = jwt.sign(
+      { userId: user._id, username: user.username },
+      JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    req.session.cookie.isLoggedIn = true;
+    req.session.cookie.token = token;
+    return res.status(200).json({
+      token: token,
+      ...user,
+    });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
+
+const logout = (req, res, next) => {
+  req.session.destroy((e) => {
+    console.log(e);
+  });
+  return res.json({ message: "Logged Out" });
+};
+
 module.exports = {
   createAccount,
+  login,
+  logout,
 };
