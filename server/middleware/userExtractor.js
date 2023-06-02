68ab3eff9977ad974f0eabf05776d6e86b15b267
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-02 14:01:17 +0530

    [server]token and user extractor middleware

---
+++ server/middleware/userExtractor.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../services/config");
+const User = require("../models/User");
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(401).json({
+      error: "User Unauthorized",
+    });
+  }
+  if (authorization.startsWith("Bearer")) {
+    const token = authorization.split(" ")[1];
+    req.token = token;
+  }
+  next();
+  return null;
+};
+
+const userExtractor = async (req, res, next) => {
+  try {
+    const payload = jwt.verify(req.token, JWT_SECRET);
+    req.user = await User.findById(payload.userId);
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(401).json({
+      error: "User Unauthorized/User not Found",
+    });
+  }
+};
+
+module.exports = {
+  tokenExtractor,
+  userExtractor,
+};
