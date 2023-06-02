7b6b87e5087571d9147850974e620bd00e0499c5
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-02 10:25:39 +0530

    [server]Login and Logout routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
-const { createAccount } = require("../controllers/auth");
+const { createAccount, login, logout } = require("../controllers/auth");
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+global.XMLHttpRequest = require("xhr2");
 
 const router = require("express").Router();
 
 router.post("/signup", upload.single("avatar"), createAccount);
+router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
