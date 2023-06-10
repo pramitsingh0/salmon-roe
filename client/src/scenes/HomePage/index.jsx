ad6636a6da4faadf53328a2a8f387e1773666610
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-10 10:40:28 +0530

    [client] Added UserWidget

--- client/src/scenes/HomePage/index.jsx
+++ client/src/scenes/HomePage/index.jsx
@@ -1,10 +1,30 @@
 import { Box } from "@mui/material";
 import NavBar from "@/scenes/NavBar";
+import UserWidget from "@/scenes/widgets/UserWidget";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  const user = useSelector((state) => state.auth.user);
   return (
     <Box>
       <NavBar />
+      <Box
+        sx={{
+          width: "100%",
+          padding: "2rem 6%",
+          display: { lg: "flex", xs: "block" },
+          gap: "0.5rem",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            flexBasis: { xs: undefined, lg: "26%" },
+          }}
+        >
+          <UserWidget userId={user._id} />
+        </Box>
+      </Box>
     </Box>
   );
 };
