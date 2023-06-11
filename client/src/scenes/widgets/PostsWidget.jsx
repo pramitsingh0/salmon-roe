ae7dc4cfa02260b6dac548b9d28bc4067c867058
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-11 14:01:11 +0530

    Post Widget

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts, fetchUserPosts } from "@/redux/postReducer";
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  useEffect(() => {
+    if (isProfile) {
+      dispatch(fetchUserPosts(userId, token));
+    } else {
+      dispatch(fetchPosts(token));
+    }
+  }, [dispatch, token, userId, isProfile]);
+  const posts = useSelector((state) => state.posts);
+  const hello = [1, 2, 3];
+  console.log(hello);
+
+  return (
+    <>
+      {posts.map((post) => (
+        <PostWidget key={post._id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostsWidget;
