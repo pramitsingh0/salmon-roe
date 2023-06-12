c1ee3132fb79017947387514c9e8416516597e58
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-12 14:34:31 +0530

    [client]FriendList widget

--- client/src/scenes/HomePage/index.jsx
+++ client/src/scenes/HomePage/index.jsx
@@ -1,12 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import NavBar from "@/scenes/NavBar";
 import UserWidget from "@/scenes/widgets/UserWidget";
 import CreatePostWidget from "@/scenes/widgets/CreatePostWidget";
 import PostsWidget from "../widgets/PostsWidget";
 import { useSelector } from "react-redux";
+import FriendsListWidget from "../widgets/FriendsListWidget";
 
 const HomePage = () => {
   const user = useSelector((state) => state.auth.user);
+  const isDesktop = useMediaQuery("(min-width:1000px)");
   return (
     <Box>
       <NavBar />
@@ -36,6 +38,12 @@ const HomePage = () => {
           <CreatePostWidget profilePath={user.profileImageUrl} />
           <PostsWidget userId={user._id} />
         </Box>
+        {isDesktop && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+            <FriendsListWidget />
+          </Box>
+        )}
       </Box>
     </Box>
   );
---
+++ client/src/scenes/widgets/FriendsListWidget.jsx
@@ -0,0 +1,66 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import Person from "@/components/Person";
+import WidgetWrapper from "@/components/WidgetWrapper";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "@/redux/authReducer";
+import axios from "axios";
+
+const FriendsListWidget = () => {
+  const dispatch = useDispatch();
+  const { palette } = useTheme();
+  const auth = useSelector((state) => state.auth);
+  const token = auth.token;
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/user/friends", null, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => setFriends(response.data))
+      .catch((e) => {
+        throw new Error(e?.message);
+      });
+  }, [token, friends]);
+
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={palette.neutral.dark}
+        variant="h5"
+        fontWeight={"500"}
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1.5rem",
+        }}
+      >
+        {friends.length ? (
+          friends.map((person) => (
+            <Person
+              key={person._id}
+              personId={person._id}
+              name={`${person.firstName} ${person.lastName}`}
+              subtitle={person.username}
+              userPicturePath={person.profileImageUrl}
+            />
+          ))
+        ) : (
+          <Typography>
+            No friends yet. Explore more to add new friends
+          </Typography>
+        )}
+      </Box>
+    </WidgetWrapper>
+  );
+};
+
+export default FriendsListWidget;
