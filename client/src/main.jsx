bfd594d7accd8b6b01e014a1f1c12b8e6d4df806
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-13 10:40:26 +0530

    [client] added axios baseurl

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
