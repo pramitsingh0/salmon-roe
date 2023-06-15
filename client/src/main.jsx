a03c8846351bf2d68577f8ab64f09bc8114c9fdd
pramitsingh0 <pramitsingh0@gmail.com>
2023-06-15 14:48:03 +0530

    [client] updated axios baseurl

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://animefreak-backend.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
